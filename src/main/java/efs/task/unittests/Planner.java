2ac9f2cb690394c5d2476558ef1b23a7526bb731
dlaska <dawidlaska888@gmail.com>
2023-05-25 19:53:53 +0200

    finished unit tests

--- src/main/java/efs/task/unittests/Planner.java
+++ src/main/java/efs/task/unittests/Planner.java
@@ -1,48 +1,61 @@
 package efs.task.unittests;
 
-class Planner {
+class Planner
+{
 
-    private int calculateProtein(int bmr) {
+    private int calculateProtein(int bmr)
+    {
         int proteinPercentage = 20;
         return (int) Math.round(bmr * proteinPercentage / 400.0);
     }
 
-    private int calculateFat(int bmr) {
+    private int calculateFat(int bmr)
+    {
         int fatPercentage = 30;
         return (int) Math.round(bmr * fatPercentage / 900.0);
     }
 
-    private int calculateCarbohydrate(int bmr) {
+    private int calculateCarbohydrate(int bmr)
+    {
         int carbohydratePercentage = 50;
         return (int) Math.round(bmr * carbohydratePercentage / 400.0);
     }
 
-    public int calculateDailyCaloriesDemand(User user, ActivityLevel level) {
+    public int calculateDailyCaloriesDemand(User user, ActivityLevel level)
+    {
         validateUserData(user);
         return (int) Math.round(calculateCaloriesDemand(user) * level.getFactor());
     }
 
-    private double calculateCaloriesDemand(User user) {
+    private double calculateCaloriesDemand(User user)
+    {
         double caloriesDemand;
-        if (user.getGender().equals(User.Gender.MALE)) {
+        if (user.getGender().equals(User.Gender.MALE))
+        {
             caloriesDemand = 66 + (13.7 * user.getWeight()) + (5 * user.getHeight() * 100) - (6.8 * user.getAge());
-        } else {
+        }
+        else
+        {
             caloriesDemand = 655 + (9.6 * user.getWeight()) + (1.8 * user.getHeight() * 100) - (4.7 * user.getAge());
         }
         return caloriesDemand;
     }
 
-    private static void validateUserData(User user) {
-        if (user.getAge() == 0 || user.getHeight() == 0.0 || user.getWeight() == 0.0 || user.getGender() == null) {
+    private static void validateUserData(User user)
+    {
+        if (user.getAge() == 0 || user.getHeight() == 0.0 || user.getWeight() == 0.0 || user.getGender() == null)
+        {
             throw new IllegalArgumentException("Do obliczenia zapotrzebowania wymagane są wszystkie dane o użytkowniku");
         }
     }
 
-    public DailyIntake calculateDailyIntake(User user) {
+    public DailyIntake calculateDailyIntake(User user)
+    {
         return calculateDailyIntake(user, ActivityLevel.LOW_ACTIVITY);
     }
 
-    private DailyIntake calculateDailyIntake(User user, ActivityLevel level) {
+    private DailyIntake calculateDailyIntake(User user, ActivityLevel level)
+    {
         int calories = calculateDailyCaloriesDemand(user, level);
         int protein = this.calculateProtein(calories);
         int fat = this.calculateFat(calories);
--- src/test/java/efs/task/unittests/FitCalculatorTest.java
+++ src/test/java/efs/task/unittests/FitCalculatorTest.java
@@ -4,14 +4,10 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.function.Executable;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
-import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.function.BiFunction;
-import java.util.function.Function;
 
 import static org.junit.jupiter.api.Assertions.*;
 
